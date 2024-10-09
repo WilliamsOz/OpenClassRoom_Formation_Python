954e195f7d71e96461a68dd3f05bf11d2af23668
WilliamsOz <williams.ozserttas05@gmail.com>
2024-10-09 13:26:29 +0200

    End of 4th exercice

---
+++ ex03.py
@@ -0,0 +1,7 @@
+fruits = ["pomme", "banane", "orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1] = "ananas"
+print("La liste contient ", len(fruits), " éléments")
+fruits.sort()
+print(fruits)
