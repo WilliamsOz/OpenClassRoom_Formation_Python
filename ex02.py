7f20b25434227f39c84e7693e6f159e419bde517
WilliamsOz <williams.ozserttas05@gmail.com>
2024-10-09 13:11:10 +0200

    End of third exercice

---
+++ ex02.py
@@ -0,0 +1,11 @@
+nom = "Williams Ozserttas"
+age = 28
+taille = 1.80
+est_etudiant = True
+
+print(f"Je m'appelle {nom}, j'ai {age}, je fais {taille}cm, suis-je étudiant ? {est_etudiant}")
+
+print(f"{type(nom)}")
+print(f"{type(age)}")
+print(f"{type(taille)}")
+print(f"{type(est_etudiant)}")
